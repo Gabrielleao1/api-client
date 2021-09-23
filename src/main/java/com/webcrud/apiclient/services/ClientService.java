8980cfbc92ddd6900144f9695ce07f7c95a2b315
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2021-09-22 21:47:13 -0300

    Delete client created

--- src/main/java/com/webcrud/apiclient/services/ClientService.java
+++ src/main/java/com/webcrud/apiclient/services/ClientService.java
@@ -3,6 +3,8 @@ package com.webcrud.apiclient.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -48,6 +50,19 @@ public class ClientService {
 			return new ClientDTO(entity);
 		}
 		catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw e;
+		}
+		catch (DataIntegrityViolationException e) {
 			throw e;
 		}
 	}
